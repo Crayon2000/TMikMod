a1dd7c49ac812846773f64a6ad6ac734d51ba90e
Crayon2000 <crayon1@rocketmail.com>
2017-05-20 01:39:55 -0400

    Add Android target to FireMonkey demo project

---
+++ ResourceIncluder.pas
@@ -0,0 +1,11 @@
+unit ResourceIncluder;
+
+interface
+
+implementation
+
+// If you get a E1026 File not found error.
+// Compile the rc file manually before compiling this file.
+{$R 'ModPlayerResources.res'}
+
+end.
